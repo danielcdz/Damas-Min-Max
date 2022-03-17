ed81c4473bd4b5812f644aee83ee5e144226ef6e
Esteban Cruz <xllestebanllx@estudiantec.cr>
2022-03-17 14:39:41 -0600

    mejoramiento

--- Minimax.rkt
+++ Minimax.rkt
@@ -3,12 +3,8 @@
 (define-struct EstadoTablero (posicionesFichas))
 (define-struct Nodo (hijos estadoTablero utilidad) #:transparent #:mutable ) ;Nodo estado de tablero
 
-(define (Alpha-Beta-Search nodo)
-(let ([v (MAX nodo -inf.0 +inf.0)])
-    (for ([hijo (Nodo-hijos nodo)]#:final (= (Utility hijo)(Utility v)))
-        (set! v (if (= (Utility hijo)(Utility v)) hijo v)))
-    v))
-
+(define (Result raiz v) (for ([hijo (Nodo-hijos raiz)]#:when (= (Utility hijo)(Utility v))) (set! v hijo)) v)
+(define (Alpha-Beta-Search nodo) (Result nodo (MAX nodo -inf.0 +inf.0)))
 (define (esTerminal? estado) (empty? (Nodo-hijos estado)))
 
 (define (MAX estado alpha beta)
@@ -16,7 +12,8 @@
         (let ([computedLevel (for/list([hijo (Nodo-hijos estado)])(MIN hijo alpha beta))])
             (define v (first computedLevel))
             (for ([minedHijo computedLevel]
-                #:break (>= (Utility minedHijo) beta))
+                #:final (>= (Utility minedHijo) beta))
+                (print (if (>= (Utility minedHijo) beta)minedHijo beta))
                 (set! v (if (>(Utility minedHijo)(Utility v)) minedHijo v))
                 (set-Nodo-utilidad! estado (if (>(Utility minedHijo)(Utility v)) (Utility minedHijo) (Utility v)))
                 (set! alpha (max alpha (Utility v))))
@@ -28,7 +25,8 @@
         (let ([computedLevel (for/list ([hijo (Nodo-hijos estado)])(MAX hijo alpha beta))])
             (define v (first computedLevel))
             (for ([maxedHijo computedLevel]
-                #:break (<= (Utility maxedHijo) alpha))
+                #:final (<= (Utility maxedHijo) alpha))
+                (print (if (<= (Utility maxedHijo) alpha) maxedHijo alpha))
                 (set! v (if (<(Utility maxedHijo)(Utility v)) maxedHijo v))
                 (set-Nodo-utilidad! estado (if (<(Utility maxedHijo)(Utility v))(Utility maxedHijo)(Utility v)))
                 (set! beta (min beta (Utility v))))
--- pruebasMinimax.rkt
+++ pruebasMinimax.rkt
@@ -1,6 +1,16 @@
 ;script to run after pruebas.rkt
 (define p1 (Nodo (list(Nodo (list(Nodo '() '() 1)(Nodo '() '() 2))'() -inf.0)(Nodo (list (Nodo '() '() 2) (Nodo '() '() 4)) '() -inf.0)) '() -inf.0))
-(define p2 (Nodo (list  
+(define p2 (Nodo (list 
+            (Nodo (list 
+                (Nodo '() '() 8)
+                (Nodo '() '() 8)
+            ) '() 4) 
+            (Nodo (list 
+                (Nodo '() '() 5)
+                (Nodo '() '() 7)
+            ) '() 5)
+        ) '() 4))
+(define p3 (Nodo (list  
 ;MAX
     (Nodo (list             
     ;MIN
@@ -38,17 +48,9 @@
                 (Nodo '() '() 9)
             ) '() 5)
         ) '() 2)
-        (Nodo (list 
-            (Nodo (list 
-                (Nodo '() '() 4)
-                (Nodo '() '() 5)
-            ) '() 4) 
-            (Nodo (list 
-                (Nodo '() '() 5)
-                (Nodo '() '() 7)
-            ) '() 5)
-        ) '() 4)
+        p2
     )'() -inf.0)
 )'() -inf.0))
 (Alpha-Beta-Search p1)
 (Alpha-Beta-Search p2)
+(Alpha-Beta-Search p3)
